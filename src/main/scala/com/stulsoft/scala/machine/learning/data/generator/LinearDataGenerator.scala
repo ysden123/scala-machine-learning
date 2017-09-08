1fc72719c5f415cbcc6d4e821cb1b45bd6c2daa2
ysden123 <ysden123@gmail.com>
2017-09-08 13:40:11 +0300

    deviation

--- src/main/scala/com/stulsoft/scala/machine/learning/data/generator/LinearDataGenerator.scala
+++ src/main/scala/com/stulsoft/scala/machine/learning/data/generator/LinearDataGenerator.scala
@@ -7,6 +7,8 @@ package com.stulsoft.scala.machine.learning.data.generator
 import com.stulsoft.scala.machine.learning.chart.XYLineChart
 import org.jfree.ui.RefineryUtilities
 
+import scala.util.Random
+
 /** Linear Data Generator
   *
   * @author Yuriy Stul
@@ -15,7 +17,9 @@ case class LinearDataGenerator(k: Double, offset: Double, deviation: Double, ste
   def generateData(): Seq[(Double, Double)] = {
     def getX(i: Int): Double = step * i
 
-    def getY(x: Double): Double = offset + k * x
+    def getY(x: Double): Double = {
+      offset + k * x + deviation * Random.nextDouble() * (if (Random.nextBoolean()) 1 else -1)
+    }
 
     for {i <- 1 to n}
       yield (getX(i), getY(getX(i)))
@@ -26,7 +30,7 @@ case class LinearDataGenerator(k: Double, offset: Double, deviation: Double, ste
   * Test app for LinearDataGenerator
   */
 object LinearDataGeneratorTest extends App {
-  val data = LinearDataGenerator(2.0, 0.0, 1.5, 2.0, 10).generateData()
+  val data = LinearDataGenerator(2.0, 0.0, 10, 2.0, 100).generateData()
   val chart = new XYLineChart("Data generator", "Linear data generator", "simple line", data)
   chart.pack()
   RefineryUtilities.centerFrameOnScreen(chart)
